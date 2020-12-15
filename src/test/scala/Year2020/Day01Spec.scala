9931c7ee927e4cfbe99124f5284c95e06cca83e1
Jeremy Clough <talesin@users.noreply.github.com>
2020-12-15 10:54:47 -0800

    Day1 (#1)
    * remove bloop
    
    * updated gitignore
    
    * added file input for day1 answer

--- src/test/scala/Year2020/Day01Spec.scala
+++ src/test/scala/Year2020/Day01Spec.scala
@@ -1,15 +1,36 @@
 package Year2020
 
 import org.scalacheck._
+import org.scalatest.flatspec._
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.scalacheck._
 
-object Day01Spec extends Properties("ReportRepairs") {
-  val lessThan1010 = Gen.nonEmptyListOf(Gen.choose(1, 1019))
+import scala.io.Source
 
-  property("find two that add to 2020 then squared") = Prop.forAll(lessThan1010) { (xs: List[Int]) =>
-    val x = 2020 - xs.head
-    val expected: Option[Int] = Some(x * xs.head)
-    val result = Day01.reportRepair(x :: xs)
-//    println(s"$result == $expected")
-    result == expected
+class Day01Spec extends AnyFlatSpec with Matchers with ScalaCheckPropertyChecks {
+  private val lessThan1010 = Gen.nonEmptyListOf(Gen.choose(1, 1019))
+
+  "reportRepair" should "find two that add to 2020 then multiply" in {
+    forAll(lessThan1010) { (xs: List[Int]) =>
+      val x = 2020 - xs.head
+      val expected: Option[Int] = Some(x * xs.head)
+      val result = Day01.reportRepair(x :: xs)
+      //    println(s"$result == $expected")
+      result shouldBe expected
+    }
+  }
+
+  "reportRepair" should "using provided file list find two that add to 2020 then multiply them" in {
+    val filename = "./src/test/scala/Year2020/Day01-input.txt"
+    val xs = Source.fromFile(filename)
+      .getLines
+      .map(_.toIntOption)
+      .filter(_.isDefined)
+      .map(_.get)
+      .toList
+
+    val result = Day01.reportRepair(xs)
+
+    result shouldBe Some(270144)
   }
 }
